297b8cf63dbb5dc830fa5f2abf362657991d1bd4
GetSya <arasyarafi02@gmail.com>
2026-09-10 12:01:01 +0700

    ?

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ import { watchFile, unwatchFile } from 'fs';
 import chalk from 'chalk';
 import { fileURLToPath } from 'url';
 
-global.pairingNumber = 6285183221210;
+global.pairingNumber = 6288213292687;
 global.pairingCode = 'RSYARAFI'; // custom pairing code (8 karakter, tanpa tanda -). Ditampilkan sebagai RSYA-RAFI
 global.owner = [['6288214772441', 'ARASYA', true]];
 global.mods = [];
